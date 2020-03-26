5c338755986980795c65485352531b668ff20dfd
jingren <renjing13@meituan.com>
2020-03-26 18:37:56 +0800

    stack

--- src/main/java/com/leetcode/Stack/Solution59_I.java
+++ src/main/java/com/leetcode/Stack/Solution59_I.java
@@ -1,5 +1,11 @@
 package com.leetcode.Stack;
 
+import com.google.common.collect.Lists;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA
  * Project: 滑动窗口的最大值
@@ -11,7 +17,38 @@ package com.leetcode.Stack;
  */
 public class Solution59_I {
 
+    LinkedList queue = new LinkedList();
+    LinkedList maxQueue = new LinkedList();
+    int max = 0;
+
     public int[] maxSlidingWindow(int[] nums, int k) {
-        return null;
+
+        for (int i = 0; i < k; i++) {
+            if(queue.isEmpty()){
+                maxQueue.add(nums[i]);
+            }else{
+                maxQueue.add(Math.max((Integer) maxQueue.getFirst(), nums[i]));
+            }
+            queue.add(nums[i]);
+        }
+        int[] result = new int[nums.length - k + 1];
+        for (int i = k; i < nums.length; i++) {
+            int j = 0;
+            if (!maxQueue.isEmpty()) {
+                result[j++] = (Integer) maxQueue.getFirst();
+            }
+            queue.add(nums[i]);
+            maxQueue.add(Math.max((Integer) maxQueue.getFirst(), nums[i]));
+            queue.removeFirst();
+            maxQueue.removeFirst();
+        }
+
+        return result;
+    }
+    
+    public static void main(String[] args){
+        Solution59_I test = new Solution59_I();
+        int [] nums = new int[]{1,3,-1,-3,5,3,6,7};
+        int [] result = test.maxSlidingWindow(nums, 3);
     }
 }
