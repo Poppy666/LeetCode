69c4ae8067103919d98d8bef082d6ab221244d85
jingren <renjing13@meituan.com>
2020-05-18 20:32:56 +0800

    solution26

---
+++ src/main/java/com/leetcode/TwoPointers/Solution28.java
@@ -0,0 +1,19 @@
+package com.leetcode.TwoPointers;
+
+/**
+ * Created with IntelliJ IDEA
+ * Project:给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+ * 输入: haystack = "hello", needle = "ll"
+ * 输出: 2
+ * Author: jingren
+ * Date: 2020/5/18
+ * Time: 上午9:35
+ */
+public class Solution28 {
+
+    public int strStr(String haystack, String needle) {
+        //定义：空字符串返回0？
+        return 0;
+    }
+
+}
